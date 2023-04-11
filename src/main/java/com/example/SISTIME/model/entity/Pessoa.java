b666d6087f7120eef8b1c2a4b3ed2f67f5dea8d1
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-11 19:58:35 -0300

    Entidade pessoa

--- src/main/java/com/example/SISTIME/model/entity/Pessoa.java
+++ src/main/java/com/example/SISTIME/model/entity/Pessoa.java
@@ -1,10 +1,14 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -13,8 +17,20 @@ import lombok.NoArgsConstructor;
 public class Pessoa {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String login;
     private String senha;
     private String nome;
+    private String email;
+    private LocalDate dataNascimento;
+    private String cpf;
+    private String telefone;
+    private String logradouro;
+    private Integer numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
 }
