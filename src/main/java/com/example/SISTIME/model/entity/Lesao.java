8116767f92d3b3f7c319bcbf31e754caa0fbbdd4
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-11 20:06:52 -0300

    Entidade Lesao

---
+++ src/main/java/com/example/SISTIME/model/entity/Lesao.java
@@ -0,0 +1,29 @@
+package com.example.SISTIME.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Lesao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+    private String gravidade;
+    private String tratamentoIndicado;
+    private LocalDate inicio;
+    private LocalDate fim;
+}
