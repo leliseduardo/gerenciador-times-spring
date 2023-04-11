e28cd43e26dd0dd793c514f8bbc2319615ce8219
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-11 20:59:39 -0300

    Relacionamento de scouts da partida

--- src/main/java/com/example/SISTIME/model/entity/ScoutsPardida.java
+++ src/main/java/com/example/SISTIME/model/entity/ScoutsPardida.java
@@ -1,9 +1,6 @@
 package com.example.SISTIME.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,4 +19,7 @@ public class ScoutsPardida {
     private int impedimentos;
     private int cruzamentos;
     private int escanteios;
+
+    @ManyToOne
+    private Partida partida;
 }
