708ffe1861ff0164fd36c80b4894ec5a0d0a2961
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-11 20:57:38 -0300

    Relacionamento de scouts do jogador

--- src/main/java/com/example/SISTIME/model/entity/ScoutsJogador.java
+++ src/main/java/com/example/SISTIME/model/entity/ScoutsJogador.java
@@ -1,9 +1,6 @@
 package com.example.SISTIME.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -38,6 +35,12 @@ public class ScoutsJogador {
     private int defesaDificil;
     private int defesaPenalti;
     private int golSofrido;
+
+    @ManyToOne
+    private Jogador jogador;
+
+    @ManyToOne
+    private Partida partida;
 }
 
 
