4ccdd9df5a14d190cadbcdbb1d5937c15c39f8b9
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-11 21:15:22 -0300

    Relacionamentos de partida

--- src/main/java/com/example/SISTIME/model/entity/Partida.java
+++ src/main/java/com/example/SISTIME/model/entity/Partida.java
@@ -1,9 +1,6 @@
 package com.example.SISTIME.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +21,13 @@ public class Partida {
     private LocalDate data;
     private LocalDateTime hora;
     private String placar;
+
+    @ManyToOne
+    private Campeonato campeonato;
+    @ManyToOne
+    private Estadio estadio;
+    @ManyToOne
+    private TimeAdversario timeAdversario;
+    @ManyToOne
+    private Tecnico tecnico
 }
--- src/main/java/com/example/SISTIME/model/entity/ScoutsPartida.java
+++ src/main/java/com/example/SISTIME/model/entity/ScoutsPartida.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class ScoutsPardida {
+public class ScoutsPartida {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
