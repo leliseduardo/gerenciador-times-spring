85343ffc323f798b7ed9149e6dfac125af35316a
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-12 12:00:12 -0300

    Campeonato Repository

---
+++ src/main/java/com/example/SISTIME/model/repository/CampeonatoRepository.java
@@ -0,0 +1,7 @@
+package com.example.SISTIME.model.repository;
+
+import com.example.SISTIME.model.entity.Campeonato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampeonatoRepository extends JpaRepository<Campeonato, Long> {
+}
