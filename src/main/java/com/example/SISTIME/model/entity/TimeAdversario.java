6508e2d5de6d6138fef9a7195ae3998764c3a598
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:31:47 -0300

    Validação entidade TimeAdversário

--- src/main/java/com/example/SISTIME/model/entity/TimeAdversario.java
+++ src/main/java/com/example/SISTIME/model/entity/TimeAdversario.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,8 @@ public class TimeAdversario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank
     private String nome;
+    @NotBlank
     private String photoPath;
 }
