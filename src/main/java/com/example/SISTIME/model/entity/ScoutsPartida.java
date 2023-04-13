a86edc2fa48220b68c05d2907afa9eb077a9110d
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:29:09 -0300

    Validação entidade scouts partida

--- src/main/java/com/example/SISTIME/model/entity/ScoutsPartida.java
+++ src/main/java/com/example/SISTIME/model/entity/ScoutsPartida.java
@@ -1,6 +1,8 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,11 +17,16 @@ public class ScoutsPartida {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @PositiveOrZero
     private int posseDeBola;
+    @PositiveOrZero
     private int impedimentos;
+    @PositiveOrZero
     private int cruzamentos;
+    @PositiveOrZero
     private int escanteios;
 
+    @NotNull
     @ManyToOne
     private Partida partida;
 }
