87ec4df41e2c723d4d10305fa66b04c92fbf9a69
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:26:45 -0300

    Validação entidade ScoutsJogador

--- src/main/java/com/example/SISTIME/model/entity/ScoutsJogador.java
+++ src/main/java/com/example/SISTIME/model/entity/ScoutsJogador.java
@@ -1,6 +1,8 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,30 +17,52 @@ public class ScoutsJogador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @PositiveOrZero
     private int desarmes;
+    @PositiveOrZero
     private int faltasCometidas;
+    @PositiveOrZero
     private int golContra;
+    @PositiveOrZero
     private int cartaoAmarelo;
+    @PositiveOrZero
     private int cartaoVermelho;
+    @PositiveOrZero
     private int faltaSofrida;
+    @PositiveOrZero
     private int passeIncompleto;
+    @PositiveOrZero
     private int assistencia;
+    @PositiveOrZero
     private int finalizacaoTrave;
+    @PositiveOrZero
     private int finalizacaoDefendida;
+    @PositiveOrZero
     private int finalizacaoFora;
+    @PositiveOrZero
     private int gol;
+    @PositiveOrZero
     private int impedimento;
+    @PositiveOrZero
     private int penaltiPerdido;
+    @PositiveOrZero
     private int penaltiSofrido;
+    @PositiveOrZero
     private int penaltiCometido;
+    @PositiveOrZero
     private int jogosSemSofrerGols;
+    @PositiveOrZero
     private int defesaDificil;
+    @PositiveOrZero
     private int defesaPenalti;
+    @PositiveOrZero
     private int golSofrido;
 
+    @NotNull
     @ManyToOne
     private Jogador jogador;
 
+    @NotNull
     @ManyToOne
     private Partida partida;
 }
