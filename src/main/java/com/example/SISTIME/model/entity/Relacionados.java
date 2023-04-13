c525ebfd92550b66942fa0d69af089ecdbe4eef3
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:20:34 -0300

    Validação entidade Relacionados

--- src/main/java/com/example/SISTIME/model/entity/Relacionados.java
+++ src/main/java/com/example/SISTIME/model/entity/Relacionados.java
@@ -1,6 +1,7 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,9 +20,11 @@ public class Relacionados {
     private String saiu;
     private String entrou;
 
+    @NotNull
     @ManyToOne
     private Partida partida;
 
+    @NotNull
     @ManyToOne
     private Jogador jogador;
 }
