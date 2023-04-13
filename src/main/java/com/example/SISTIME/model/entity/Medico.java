b05fe03d23409cc8a47050a33e4cf914e22d2bca
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:11:52 -0300

    Validação entidade Medico

--- src/main/java/com/example/SISTIME/model/entity/Medico.java
+++ src/main/java/com/example/SISTIME/model/entity/Medico.java
@@ -1,6 +1,7 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Medico extends Pessoa{
 
+    @NotBlank
     private String registro;
 }
