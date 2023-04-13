bef98af0b4c32c5df8308c89a3c9be2e9224d369
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:01:14 -0300

    Validação entidade Pessoa

--- src/main/java/com/example/SISTIME/model/entity/Campeonato.java
+++ src/main/java/com/example/SISTIME/model/entity/Campeonato.java
@@ -1,6 +1,8 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/example/SISTIME/model/entity/Pessoa.java
+++ src/main/java/com/example/SISTIME/model/entity/Pessoa.java
@@ -1,6 +1,9 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,18 +19,33 @@ public class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank
     private String login;
+    @NotBlank
     private String senha;
+    @NotBlank
     private String nome;
+    @NotBlank
     private String email;
+    @NotNull
     private LocalDate dataNascimento;
+    @NotBlank
     private String cpf;
+    @NotBlank
     private String telefone;
+    @NotBlank
     private String logradouro;
+    @NotNull
+    @Min(1)
     private Integer numero;
+    @NotBlank
     private String complemento;
+    @NotBlank
     private String bairro;
+    @NotBlank
     private String cidade;
+    @NotBlank
     private String uf;
+    @NotBlank
     private String cep;
 }
