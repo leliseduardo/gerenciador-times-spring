0de69754206dee13c386c520b2bd57cfd2cd97e3
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 20:13:00 -0300

    Criando LesaoService

---
+++ src/main/java/com/example/SISTIME/service/LesaoService.java
@@ -0,0 +1,56 @@
+package com.example.SISTIME.service;
+
+import com.example.SISTIME.model.entity.Lesao;
+import com.example.SISTIME.model.repository.LesaoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LesaoService {
+
+    @Autowired
+    private LesaoRepository repository;
+
+    public List<Lesao> getLesao(){
+       return repository.findAll();
+    }
+
+    public Optional<Lesao> getLesaoById(long id){
+        return repository.findById(id);
+    }
+
+    public Lesao create(@Valid Lesao lesao){
+        return repository.save(lesao);
+    }
+
+    public void delete(Lesao lesao){
+        repository.delete(lesao);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
