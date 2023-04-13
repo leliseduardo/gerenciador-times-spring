b1ee02a568d4a37a4ccf94955548e30f63f1de6c
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:08:48 -0300

    Validação entidade jogador

--- src/main/java/com/example/SISTIME/model/entity/Jogador.java
+++ src/main/java/com/example/SISTIME/model/entity/Jogador.java
@@ -1,6 +1,9 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,12 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Jogador extends Pessoa{
 
-
+    @NotBlank
     private String nacionalidade;
+    @NotNull
+    @DecimalMin("0.50")
     private float altura;
+    @NotNull
+    @DecimalMin("0.50")
     private float peso;
 }
