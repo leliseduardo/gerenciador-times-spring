dfc900c3526fb3bab4fbd231dfda55e34ee12d31
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:11:23 -0300

    Validação entidade LesaoJogador

--- src/main/java/com/example/SISTIME/model/entity/LesaoJogador.java
+++ src/main/java/com/example/SISTIME/model/entity/LesaoJogador.java
@@ -1,6 +1,7 @@
 package com.example.SISTIME.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,10 @@ public class LesaoJogador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
     @ManyToOne
     private Jogador jogador;
+    @NotNull
     @ManyToOne
     private Lesao lesao;
 }
