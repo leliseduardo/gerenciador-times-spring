7443ecf9c50defd2b3b188e836f3321decac7b02
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:31:16 -0300

    Validação entidade Temporada

--- src/main/java/com/example/SISTIME/model/entity/Temporada.java
+++ src/main/java/com/example/SISTIME/model/entity/Temporada.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,7 +22,10 @@ public class Temporada {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String descricao;
+    @NotNull
     private LocalDate dataInicio;
+    @NotNull
     private LocalDate dataFim;
 }
