62eb97d1f87222fc0e56106404a39b3d94d84e5e
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-13 19:09:46 -0300

    Validação entidade Lesao

--- src/main/java/com/example/SISTIME/model/entity/Lesao.java
+++ src/main/java/com/example/SISTIME/model/entity/Lesao.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,9 +24,15 @@ public class Lesao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank
     private String nome;
+    @NotBlank
     private String gravidade;
+    @NotBlank
     private String local;
+    @NotBlank
     private String tipo;
+    @NotNull
+    @Min(1)
     private long TempoMedioDeTratamento;
 }
