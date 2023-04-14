8c22b87b2dd61aec474c31e48a5fd3f6a03dbad1
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-14 12:47:51 -0300

    Criando TecnicoService

---
+++ src/main/java/com/example/SISTIME/service/TecnicoService.java
@@ -0,0 +1,61 @@
+package com.example.SISTIME.service;
+
+import com.example.SISTIME.model.entity.Tecnico;
+import com.example.SISTIME.model.repository.TecnicoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+
+    public List<Tecnico> getTecnico(){
+        return repository.findAll();
+    }
+
+    public Optional<Tecnico> getTecnicoById(long id){
+        return repository.findById(id);
+    }
+
+    public Tecnico create(@Valid Tecnico tecnico){
+        return repository.save(tecnico);
+    }
+
+    public void delete(Tecnico tecnico){
+        repository.delete(tecnico);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
