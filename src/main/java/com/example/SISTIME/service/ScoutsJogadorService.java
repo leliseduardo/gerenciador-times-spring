c580f4f3a49121f8c3401ce8e0419e1007d25b06
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-14 12:19:55 -0300

    Criando ScoutsJogadorService

---
+++ src/main/java/com/example/SISTIME/service/ScoutsJogadorService.java
@@ -0,0 +1,52 @@
+package com.example.SISTIME.service;
+
+import com.example.SISTIME.model.entity.ScoutsJogador;
+import com.example.SISTIME.model.repository.ScoutsJogadorRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ScoutsJogadorService {
+
+    @Autowired
+    private ScoutsJogadorRepository repository;
+
+    public List<ScoutsJogador> getScoutsJogador(){
+        return repository.findAll();
+    }
+
+    public Optional<ScoutsJogador> getScoutsJogadorById(long id){
+        return repository.findById(id);
+    }
+
+    public ScoutsJogador create(@Valid ScoutsJogador scoutsJogador){
+        return repository.save(scoutsJogador);
+    }
+
+    public void delete(ScoutsJogador scoutsJogador){
+        repository.delete(scoutsJogador);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
