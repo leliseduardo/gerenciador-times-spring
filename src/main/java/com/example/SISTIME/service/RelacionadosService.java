61c2a36e327ca904d990fb277976737e5de52f7f
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-14 12:15:45 -0300

    Criando RelacionadosService

---
+++ src/main/java/com/example/SISTIME/service/RelacionadosService.java
@@ -0,0 +1,63 @@
+package com.example.SISTIME.service;
+
+import com.example.SISTIME.model.entity.Relacionados;
+import com.example.SISTIME.model.repository.RelacionadosRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RelacionadosService {
+
+    @Autowired
+    private RelacionadosRepository repository;
+
+    public List<Relacionados> getRelacionados(){
+        return repository.findAll();
+    }
+
+    public Optional<Relacionados> getRelacionadosById(long id){
+        return repository.findById(id);
+    }
+
+    public Relacionados create(@Valid Relacionados relacionados){
+        return repository.save(relacionados);
+    }
+
+    public void delete(Relacionados relacionados){
+        repository.delete(relacionados);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
