8f3944992667d7b8a40e7c632eebc8a2a6ce4a62
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-14 12:44:22 -0300

    Criando ScoutsPartidaService

---
+++ src/main/java/com/example/SISTIME/service/ScoutsPartidaService.java
@@ -0,0 +1,57 @@
+package com.example.SISTIME.service;
+
+import com.example.SISTIME.model.entity.ScoutsPartida;
+import com.example.SISTIME.model.repository.ScoutsPartidaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ScoutsPartidaService {
+
+    @Autowired
+    private ScoutsPartidaRepository repository;
+
+    public List<ScoutsPartida> getScoutsPartida(){
+        return repository.findAll();
+    }
+
+    public Optional<ScoutsPartida> getScoutsPartidaById(long id){
+        return repository.findById(id);
+    }
+
+    public ScoutsPartida create(@Valid ScoutsPartida scoutsPartida){
+        return repository.save(scoutsPartida);
+    }
+
+    public void delete(ScoutsPartida scoutsPartida){
+        repository.delete(scoutsPartida);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
