c6656758aed1eac8fd7e26f1625a875525fa4478
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-14 12:55:18 -0300

    Criando TimeAdversarioService

---
+++ src/main/java/com/example/SISTIME/service/TimeAdversarioService.java
@@ -0,0 +1,55 @@
+package com.example.SISTIME.service;
+
+import com.example.SISTIME.model.entity.TimeAdversario;
+import com.example.SISTIME.model.repository.TimeAdversarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TimeAdversarioService {
+
+    @Autowired
+    private TimeAdversarioRepository repository;
+
+    public List<TimeAdversario> getTimeAdversario(){
+        return repository.findAll();
+    }
+
+    public Optional<TimeAdversario> getTimeAdversarioById(long id){
+        return repository.findById(id);
+    }
+
+    public TimeAdversario create(@Valid TimeAdversario timeAdversario){
+        return repository.save(timeAdversario);
+    }
+
+    public void delete(TimeAdversario timeAdversario){
+        repository.delete(timeAdversario);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
