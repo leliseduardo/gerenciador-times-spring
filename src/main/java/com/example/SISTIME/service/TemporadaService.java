e032dd11d4ad785afc687761ebb8d0a09adb47bd
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-14 12:51:32 -0300

    Criando TemporadaService

---
+++ src/main/java/com/example/SISTIME/service/TemporadaService.java
@@ -0,0 +1,57 @@
+package com.example.SISTIME.service;
+
+
+import com.example.SISTIME.model.entity.Temporada;
+import com.example.SISTIME.model.repository.TemporadaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TemporadaService {
+
+    @Autowired
+    private TemporadaRepository repository;
+
+    public List<Temporada> getTemporada(){
+        return repository.findAll();
+    }
+
+    public Optional<Temporada> getTemporadaById(long id){
+        return repository.findById(id);
+    }
+
+    public Temporada create(@Valid Temporada temporada){
+        return repository.save(temporada);
+    }
+
+    public void delete(Temporada temporada){
+        repository.delete(temporada);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
