abd199334467a6b194677a7670d6707b1855a1a9
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-17 17:06:03 -0300

    Criando EstadioController

---
+++ src/main/java/com/example/SISTIME/api/controller/EstadioController.java
@@ -0,0 +1,62 @@
+package com.example.SISTIME.api.controller;
+
+import com.example.SISTIME.api.dto.EstadioDto;
+import com.example.SISTIME.model.entity.Estadio;
+import com.example.SISTIME.service.EstadioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/estadios")
+@RequiredArgsConstructor
+public class EstadioController {
+
+    private final EstadioService service;
+
+    @GetMapping
+    public ResponseEntity get(){
+        List<Estadio> estadios = service.getEstadio();
+
+        return ResponseEntity.ok(estadios.stream().map(EstadioDto::create).collect(Collectors.toList()));
+        //return ResponseEntity.ok(estadios.stream().map(c -> EstadioDto.create(c)).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable("id") long id){
+        Optional<Estadio> estadio = service.getById(id);
+
+        if(!estadio.isPresent()){
+            return new ResponseEntity("Estádio não encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(estadio.map(EstadioDto::create));
+        //return ResponseEntity.ok(estadio.map(c -> EstadioDto.create(c)));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
