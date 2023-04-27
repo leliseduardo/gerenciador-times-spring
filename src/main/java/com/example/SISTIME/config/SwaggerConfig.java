fb146f9761263cc8462e3d1e4881cfe9be9e798f
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-04-27 16:43:13 -0300

    Configurando o Swagger

--- src/main/java/com/example/SISTIME/config/SwaggerConfig.java
+++ src/main/java/com/example/SISTIME/config/SwaggerConfig.java
@@ -1,44 +1,49 @@
-//package com.example.SISTIME.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket docket(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .useDefaultResponseMessages(false)
-//                .select()
-//                .apis(RequestHandlerSelectors
-//                        .basePackage("com.example.SISTIME.api.controller"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//
-//    private ApiInfo apiInfo(){
-//        return new ApiInfoBuilder()
-//                .title("Sistema Time Futebol")
-//                .description("API para um sistema de time de futebol")
-//                .version("1.0")
-//                .contact(contact())
-//                .build();
-//    }
-//
-//    private Contact contact(){
-//        return new Contact("Eduardo Lelis",
-//                "https://github.com/leliseduardo",
-//                "eduardo.lelis.co@gmail.com");
-//    }
-//}
+package com.example.SISTIME.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.example.SISTIME.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Sistema Time Futebol")
+                .description("API para um sistema de time de futebol")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact(){
+        return new Contact("Eduardo Lelis",
+                "https://github.com/leliseduardo",
+                "eduardo.lelis.co@gmail.com");
+    }
+}
+
+
+
+
+
